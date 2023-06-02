86895ea58031c8fe06fc6e98ed4d41d5daf59db1
ALLLV <vol.lapshin2018@gmail.com>
2023-06-02 23:43:29 +0300

    init

---
+++ JsonParsingPractice/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JsonParsingPractice;
+internal class Employee
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("phoneNumber")]
+    public string PhoneNumber { get; set; }
+
+    [JsonPropertyName("salary")]
+    public string Salary { get; set; }
+}
+
---
+++ JsonParsingPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace JsonParsingPractice;
+internal class Program
+{
+    static readonly string pathMain = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\data.json");
+    static readonly string pathNew = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\dataEdit.json");
+    static List<Employee> employees = new();
+    static async Task Main(string[] args)
+    {
+        employees = ParseJson(File.ReadAllText(pathMain));
+        foreach (var e in employees) e.PhoneNumber = PhoneFormat(e.PhoneNumber);
+        await CreateJsonView(pathNew, SortEmployees(employees, 20));
+    }
+
+    static Func<string, List<Employee>> ParseJson = json => System.Text.Json.JsonSerializer.Deserialize<List<Employee>>(json);
+    static List<Employee> SortEmployees(List<Employee> employee, int length)
+       => employees.OrderByDescending(e => int.Parse(e.Salary)).ToList().GetRange(0, length);
+    static string PhoneFormat(string phone)
+        => string.Format($"+{phone.Substring(0, 1)}({phone.Substring(1, 3)}){phone.Substring(4, 3)}-{phone.Substring(7, 2)}-{phone.Substring(9, 2)}");
+    static async Task CreateJsonView(string path, List<Employee> employees)
+    {
+        string? output = null;
+        foreach (var e in employees) output += $"{e.Name} {e.PhoneNumber} {e.Salary}\n";
+        File.WriteAllText(path, output);
+    }
+}
